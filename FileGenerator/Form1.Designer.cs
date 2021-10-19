49571a76df3be1f726781114e739dbfa49ff7f55
fabioGeorg <62179752+fabioGeorg@users.noreply.github.com>
2021-10-19 14:18:58 +0200

    Add files via upload

---
+++ FileGenerator/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace FileGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerer = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numTailleFichier = new System.Windows.Forms.NumericUpDown();
+            this.progressionGener = new System.Windows.Forms.ProgressBar();
+            this.lblProgession = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.btnAnnuler = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numTailleFichier)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGenerer
+            // 
+            this.btnGenerer.Location = new System.Drawing.Point(168, 87);
+            this.btnGenerer.Name = "btnGenerer";
+            this.btnGenerer.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerer.TabIndex = 0;
+            this.btnGenerer.Text = "Générer";
+            this.btnGenerer.UseVisualStyleBackColor = true;
+            this.btnGenerer.Click += new System.EventHandler(this.btnGenerer_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Taille du fichier";
+            // 
+            // numTailleFichier
+            // 
+            this.numTailleFichier.Location = new System.Drawing.Point(143, 36);
+            this.numTailleFichier.Maximum = new decimal(new int[] {
+            1874919424,
+            2328306,
+            0,
+            0});
+            this.numTailleFichier.Name = "numTailleFichier";
+            this.numTailleFichier.Size = new System.Drawing.Size(329, 20);
+            this.numTailleFichier.TabIndex = 3;
+            this.numTailleFichier.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numTailleFichier.ValueChanged += new System.EventHandler(this.numTailleFichier_ValueChanged);
+            // 
+            // progressionGener
+            // 
+            this.progressionGener.Location = new System.Drawing.Point(12, 177);
+            this.progressionGener.Name = "progressionGener";
+            this.progressionGener.Size = new System.Drawing.Size(528, 23);
+            this.progressionGener.Step = 1;
+            this.progressionGener.TabIndex = 4;
+            // 
+            // lblProgession
+            // 
+            this.lblProgession.AutoSize = true;
+            this.lblProgession.Location = new System.Drawing.Point(260, 158);
+            this.lblProgession.Name = "lblProgession";
+            this.lblProgession.Size = new System.Drawing.Size(30, 13);
+            this.lblProgession.TabIndex = 5;
+            this.lblProgession.Text = "0 / 1";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // btnAnnuler
+            // 
+            this.btnAnnuler.Location = new System.Drawing.Point(307, 87);
+            this.btnAnnuler.Name = "btnAnnuler";
+            this.btnAnnuler.Size = new System.Drawing.Size(75, 23);
+            this.btnAnnuler.TabIndex = 6;
+            this.btnAnnuler.Text = "Annuler";
+            this.btnAnnuler.UseVisualStyleBackColor = true;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(12, 145);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(43, 13);
+            this.lblStatus.TabIndex = 7;
+            this.lblStatus.Text = "Status: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 212);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnAnnuler);
+            this.Controls.Add(this.lblProgession);
+            this.Controls.Add(this.progressionGener);
+            this.Controls.Add(this.numTailleFichier);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGenerer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "FileGenerator";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numTailleFichier)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numTailleFichier;
+        private System.Windows.Forms.ProgressBar progressionGener;
+        private System.Windows.Forms.Label lblProgession;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button btnAnnuler;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ FileGenerator/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Drawing;
+
+namespace FileGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+  
+        static int nb = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnAnnuler.Enabled = false;
+            backgroundWorker1.WorkerReportsProgress = true;
+            backgroundWorker1.WorkerSupportsCancellation = true;
+        }
+
+        private void btnGenerer_Click(object sender, EventArgs e)
+        {
+            if(backgroundWorker1.IsBusy != true)
+            {
+                btnGenerer.Enabled = false;
+                numTailleFichier.Enabled = false;
+                btnAnnuler.Enabled = true;
+
+                lblStatus.ForeColor = Color.Black;
+                lblStatus.Text = "Status: ";
+
+                backgroundWorker1.RunWorkerAsync();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Environment.Exit(1);
+        }
+
+        private void numTailleFichier_ValueChanged(object sender, EventArgs e)
+        {
+            lblProgession.Text = "0 / " + numTailleFichier.Value.ToString();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            string tmp = "";
+            if (Convert.ToUInt64(numTailleFichier.Value) >= 100 && Convert.ToUInt64(numTailleFichier.Value) <= 1000)
+            {
+                for (UInt64 i = 0; i < 100; i++)
+                    tmp += 'a';
+            }
+            else if (Convert.ToUInt64(numTailleFichier.Value) >= 1001 && Convert.ToUInt64(numTailleFichier.Value) <= 10000)
+            {
+                for (UInt64 i = 0; i < 1000; i++)
+                    tmp += 'a';
+            }
+            else if (Convert.ToUInt64(numTailleFichier.Value) >= 10001 && Convert.ToUInt64(numTailleFichier.Value) <= 100000)
+            {
+                for (UInt64 i = 0; i < 10000; i++)
+                    tmp += 'a';
+            }
+            else if (Convert.ToUInt64(numTailleFichier.Value) >= 100001 && Convert.ToUInt64(numTailleFichier.Value) <= 1000000)
+            {
+                for (UInt64 i = 0; i < 100000; i++)
+                    tmp += 'a';
+            }
+            else if (Convert.ToUInt64(numTailleFichier.Value) > 100000)
+            {
+                for (UInt64 i = 0; i < 1000000; i++)
+                    tmp += 'a';
+            }
+            else
+            {
+                tmp = "a";
+            }
+
+            using (StreamWriter sw = new StreamWriter("generated-" + nb + ".txt"))
+            {
+                for (UInt64 i = 1; i <= Convert.ToUInt64(numTailleFichier.Value) / Convert.ToUInt64(tmp.Length); i++)
+                {
+                    sw.Write(tmp);
+                    sw.Flush();
+                    Application.DoEvents();
+
+                    if(worker.CancellationPending)
+                    {
+                        worker.CancelAsync();
+                    }
+
+                    //lblProgession.Text = Convert.ToString(i * 10) + " / " + numTailleFichier.Value.ToString();
+                }
+            }
+            nb++;
+            
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            btnGenerer.Enabled = true;
+            numTailleFichier.Enabled = true;
+            btnAnnuler.Enabled = false;
+
+            if(e.Cancelled)
+            {
+                lblStatus.ForeColor = Color.Red;
+                lblStatus.Text = "Status: Annulé";
+            }
+            else if(e.Error != null)
+            {
+                lblStatus.ForeColor = Color.Red;
+                lblStatus.Text = "Status: Erreur: " + e.Error.Message;
+            }
+            else
+            {
+                lblStatus.ForeColor = Color.Green;
+                lblStatus.Text = "Status: Terminé";
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+        {
+            
+        }
+    }
+}
